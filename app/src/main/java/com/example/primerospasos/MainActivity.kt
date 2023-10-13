4455a84ecdb9997a6fd486a19f8952022d39b8ad
acascoc098 <acascoc098@g.educaand.es>
2023-10-13 12:37:23 +0200

    Muestra allez por pantalla, pero dura poco.

--- app/src/main/java/com/example/primerospasos/MainActivity.kt
+++ app/src/main/java/com/example/primerospasos/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.primerospasos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Cositas layout
-        setContentView(R.layout.activity_main)
+        //Cositas layout, cargar en la interfaz gráfica
+        setContentView(R.layout.activity_main)//R.id es para elementos View, no para layour que es un contenedor
+
+        //Usi del elemento que hemos añadido en strngs.xml
+        val nombre = getString(R.string.allez)
+        Toast.makeText(this, "Minombre es $nombre", Toast.LENGTH_LONG).show()//Para mostrar por pantalla
     }
 }
